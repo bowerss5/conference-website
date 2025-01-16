b618e48ddb08db0e38157562d52e8583350c35c2
bowerss5 <bowerss5@miamioh.edu>
2025-01-16 17:21:56 -0500

    implemented cohort page code from last years website

---
+++ js/cohort.js
@@ -0,0 +1,63 @@
+  $(document).ready(() => {
+    parseCsv();
+  });
+
+  function parseCsv() {
+    $.ajax({
+      url: '../data/old.csv',
+      method: 'GET',
+      type: 'text',
+    }).then(function (data) {
+      const { data: lines } = Papa.parse(data);
+      lines.slice(1).map((line, idx) => {
+        const [_, _1, bio, resp, linkedin, name, img, major] = line;
+        renderBio(idx, bio, resp, linkedin, name, img, major);
+      });
+    }).catch(function (data) {
+      console.log('failed', data);
+    });
+  }
+
+  function renderBio(id, bio, resp, linkedin, name, img, major) {
+    $(".bios").append(
+      `
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card">
+            <img src="${'https://embrace-ai-2024.s3.us-west-2.amazonaws.com/' + img}" class="card-img-top bio-img" alt="bio">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${major}</p>
+              <div class="accordion accordion-flush" id="accordionExample"> 
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne${id}" aria-expanded="true" aria-controls="collapseOne${id}">
+                      Bio
+                    </button>
+                  </h2>
+                  <div id="collapseOne${id}" class="accordion-collapse collapse" aria-labelledby="headingOne">
+                    <div class="accordion-body">
+                      ${bio}
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo${id}" aria-expanded="false" aria-controls="collapseTwo${id}">
+                      How are you using and embracing AI?
+                    </button>
+                  </h2>
+                  <div id="collapseTwo${id}" class="accordion-collapse collapse" aria-labelledby="headingTwo">
+                    <div class="accordion-body">
+                      ${resp}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a href=${linkedin}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                                              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+              </svg></a>
+            </div>
+          </div>
+        </div>
+      `
+    );}
